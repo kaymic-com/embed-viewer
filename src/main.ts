70a71c24869d11c07cddb321482426693a79bf13
Kevin McCarthy <kjmagic13@gmail.com>
2022-04-09 15:37:45 -0400

    gtag prettierrc

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from "vue"
 import App from "./App.vue"
+import VueGtag, { pageview } from "vue-gtag"
 
 import { router } from "./router"
 
-createApp(App).use(router).mount("#app")
+router.afterEach((to) => {
+  pageview({ page_path: to.fullPath, page_title: to.name?.toString() })
+})
+
+createApp(App)
+  .use(router)
+  .use(VueGtag, {
+    config: {
+      id: "G-FK8QWQRYS2",
+    },
+  })
+  .mount("#app")
